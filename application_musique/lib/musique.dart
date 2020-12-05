63cf0d71980f55b1140e8712faaf0176243d6cd1
Allan Boite <allan.boite@gmail.com>
2020-12-06 00:33:27 +0100

    ajout de code et d'extension

---
+++ application_musique/lib/musique.dart
@@ -0,0 +1,15 @@
+class Musique {
+  //création de la class Musique et des variables que l'on va utiliser à l'intérieur
+  String titre;
+  String auteur;
+  String image;
+  String musiqueURL;
+
+  Musique(String titre, String auteur, String image, String musiqueURL) {
+    //Instancie la class Musique POO
+    this.titre = titre;
+    this.auteur = auteur;
+    this.image = image;
+    this.musiqueURL = musiqueURL;
+  }
+}
